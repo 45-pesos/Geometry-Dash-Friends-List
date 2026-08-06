bae1462357dacab3ab03b2cd6e6983d6406923c3
45-pesos <finosenoresycaquita666@gmail.com>
2026-08-06 14:00:24 -0600

    Add files via upload

---
+++ Geometry Dash Friends List/js/list.js
@@ -0,0 +1,483 @@
+const d = {
+	"list": [
+		/*=================================================================================*/
+		{
+			"vids": [
+				{
+					"user": "Rasvaninx",
+					"link": "https://youtu.be/-y8Pj1DBs7U?si=G2G9bnR0sRLGJks3",
+					"percent": 100,
+					"hz": "n/a"
+				},
+				
+			],
+			"name": "Sonic Wave",
+                       "author": "UnboxingNJ",
+                       "more": "Cyclic",
+                       "id": 26681070,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 65,
+                       "verificationVid": "https://www.youtube.com/watch?v=qmVuv6Znz80",
+                       "key": 0
+		},
+	
+		/*=================================================================================*/
+		{
+			"vids": [
+	
+			],
+			"name": "Infernal Abyss",
+                       "author": "Rasvaninx",
+                       "more": "Ggb0y",
+                       "id": 27379546,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 70,
+                       "verificationVid": "https://www.youtube.com/watch?v=OSlBFtnbiXg",
+                       "key": 0
+		},
+/*=================================================================================*/
+		{
+			"vids": [
+	
+			],
+			"name": "Blade of Justice",
+                       "author": "UnboxingNJ",
+                       "more": "Manix648 & more",
+                       "id": 35448603,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 75,
+                       "verificationVid": "https://youtu.be/2ri_RVfwMUc?si=cPlRA_WNKzMnaylC",
+                       "key": 0
+		},
+/*=================================================================================*/
+			{
+			"vids": [
+	 
+			],
+			"name": "Phobos",
+                       "author": "UnboxingNJ",
+                       "more": "KrmaL",
+                       "id": 19759411,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 62,
+                       "verificationVid": "https://youtu.be/H5AZw1Ybypk?si=vZlINm2TB1TgnP2X",
+                       "key": 0
+		},
+/*=================================================================================*/
+			{
+			"vids": [
+	 {
+					"user": "Rasvaninx",
+					"link": "https://youtu.be/GsU1DQ7QSo8?si=pxnQfp7td8umxXQ33",
+					"percent": 100,
+					"hz": "n/a"
+				},
+			],
+			"name": "Bloodbath",
+                       "author": "UnboxingNJ",
+                       "more": "Riot & more",
+                       "id": 10565740,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 71,
+                       "verificationVid": "https://youtu.be/gsKwbvEHP5M?si=VIQcq6ctRyWEm2gl",
+                       "key": 0
+		},
+	/*=================================================================================*/
+			{
+			"vids": [
+	 
+			],
+			"name": "Conical Depression",
+                       "author": "UnboxingNJ",
+                       "more": "KrmaL",
+                       "id": 17711004,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 69,
+                       "verificationVid": "",
+                       "key": 0
+		},
+		/*=================================================================================*/
+			{
+			"vids": [
+	 {
+					"user": "Rasvaninx",
+					"link": "https://youtu.be/9lrcLwlVS0c?si=u5B8eLHw799vlcIL",
+					"percent": 100,
+					"hz": "n/a"
+				},
+			],
+			"name": "AfterMath",
+                       "author": "UnboxingNJ",
+                       "more": "Exenity",
+                       "id": 25610878,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 69,
+                       "verificationVid": "",
+                       "key": 0
+		},
+		/*=================================================================================*/
+			{
+			"vids": [
+	 {
+					"user": "45 pesos",
+					"link": "https://youtu.be/2cYWG2HfKrE?si=aMYxwajGv5QnBQ1E",
+					"percent": 99,
+					"hz": "180Hz"
+				},
+			],
+			"name": "Heartbeat",
+                       "author": "UnboxingNJ",
+                       "more": "KrmaL",
+                       "id": 20321297,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 69,
+                       "verificationVid": "https://youtu.be/V48p2B6xQy0?si=BQDpfpvFcbiW-r78",
+                       "key": 0
+		},
+			/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "The Ultimate Phase",
+                       "author": "UnboxingNJ",
+                       "more": "AndromedaGMD & more",
+                       "id": 7174110,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 72,
+                       "verificationVid": "",
+                       "key": 0
+		},
+				/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Falling Up",
+                       "author": "UnboxingNJ",
+                       "more": "KrmaL",
+                       "id": 24301797,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 60,
+                       "verificationVid": "https://youtu.be/1IqLT3w8nV8?si=jYHcqBupEmnp0-T_",
+                       "key": 0
+		},
+					/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Heart attack",
+                       "author": "UnboxingNJ",
+                       "more": "KrmaL",
+                       "id": 89190831,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 66,
+                       "verificationVid": "https://youtu.be/O6oty2CwFfM?si=xcmzMFFTmiV3rUsz",
+                       "key": 0
+		},
+						/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Deception Dive",
+                       "author": "UnboxingNJ",
+                       "more": "Rustam & more",
+                       "id": 34379100,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 66,
+                       "verificationVid": "",
+                       "key": 0
+		},
+							/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Reanimate",
+                       "author": "UnboxingNJ",
+                       "more": "Ilmn",
+                       "id": 80335620,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 70,
+                       "verificationVid": "https://youtu.be/CeJukvCcG4Q?si=k00UCPQu84SWvDhw",
+                       "key": 0
+		},
+									/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Niwa",
+                       "author": "UnboxingNJ",
+                       "more": "Teno",
+                       "id": 87425029,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 70,
+                       "verificationVid": "",
+                       "key": 0
+		},
+		
+						/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Cataclysm",
+                       "author": "UnboxingNJ",
+                       "more": "Ggb0y",
+                       "id": 3979721,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 66,
+                       "verificationVid": "https://youtu.be/TSLnLf-CZHI?si=zBkAv6cNPJT3bxyN",
+                       "key": 0
+		},
+									/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Tantrum",
+                       "author": "TomasChikito",
+                       "more": "Danzmen",
+                       "id": 61910810,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 68,
+                       "verificationVid": "https://youtu.be/TSLnLf-CZHI?si=zBkAv6cNPJT3bxyN",
+                       "key": 0
+		},	
+		/*=================================================================================*/
+		{
+			"vids": [
+
+			],
+			"name": "Acu",
+                       "author": "UnboxingNJ",
+                       "more": "Neigefeu",
+                       "id": 61079355,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 61,
+                       "verificationVid": "",
+                       "key": 0
+		},
+		/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Red World",
+                       "author": "UnboxingNJ",
+                       "more": "saRy",
+                       "id": 2374518,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 78,
+                       "verificationVid": "",
+                       "key": 0
+		},
+		/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Leyak",
+                       "author": "TomasChikito",
+                       "more": "EnZore",
+                       "id": 61137742,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 74,
+                       "verificationVid": "https://youtu.be/1zJsaXjuvqc?si=JVmGVZelQ5Mcn1r0",
+                       "key": 0
+		},
+		/*=================================================================================*/
+			{
+			"vids": [
+	 {
+					"user": "45 pesos",
+					"link": "https://youtu.be/8ei583UzMP0?si=VexVFssQtBCfpF0Y",
+					"percent": 100,
+					"hz": "180Hz"
+				},
+			],
+			"name": "BuTiTi II",
+                       "author": "UnboxingNJ",
+                       "more": "JonathanGD",
+                       "id": 37259527,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 66,
+                       "verificationVid": "https://youtu.be/HCXVuF5CTh4?si=1kvqwxh9NsOgGk-I",
+                       "key": 0
+		},
+
+		/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Magma Bound",
+                       "author": "TomasChikito",
+                       "more": "ScorchVx",
+                       "id": 56568010,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 71,
+                       "verificationVid": "",
+                       "key": 0
+		},
+		/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Windy Landscape",
+                       "author": "TomasChikito",
+                       "more": "Woogi1411",
+                       "id": 4957691,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 68,
+                       "verificationVid": "",
+                       "key": 0
+		},
+		/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "CraZy II",
+                       "author": "Rasvaninx",
+                       "more": "DavJT",
+                       "id": 47620786,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 66,
+                       "verificationVid": "https://youtu.be/wcCxKAg0qIk?si=GghUpXDEbaT48ZuN",
+                       "key": 0
+		},
+		/*=================================================================================*/
+			{
+			"vids": [
+	 {
+					"user": "Rasvaninx",
+					"link": "https://youtu.be/NtjNS131UIc?si=iGtSpyHq7wZv6y2-",
+					"percent": 100,
+					"hz": "n/a"
+				},
+			],
+			"name": "Future Funk",
+                       "author": "45 pesos",
+                       "more": "JonathanGD",
+                       "id": 44062068,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 62,
+                       "verificationVid": "https://youtu.be/40BrUXHj5nU?si=Qk7mFgB4WVDXIPwp",
+                       "key": 0
+		},
+	/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Different Crescent",
+                       "author": "TomasChikito",
+                       "more": "Danke",
+                       "id": 62857306,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 75,
+                       "verificationVid": "",
+                       "key": 0
+		},
+			/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "CraZy",
+                       "author": "Rasvaninx",
+                       "more": "DavJT",
+                       "id": 40945673,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 65,
+                       "verificationVid": "https://youtu.be/Iagqn-7K6FQ?si=65EkE6Wy1QTA-Ebd",
+                       "key": 0
+		},
+			/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "Aleph 0",
+                       "author": "Blurry",
+                       "more": "Shiryuu7",
+                       "id": 55429960,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 70,
+                       "verificationVid": "",
+                       "key": 0
+		},
+			/*=================================================================================*/
+			{
+			"vids": [
+
+			],
+			"name": "PG Clubstep",
+                       "author": "Rasvaninx",
+                       "more": "pg1004",
+                       "id": 513137,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 64,
+                       "verificationVid": "",
+                       "key": 0
+		},
+
+
+		/*=================================================================================*/
+			{
+			"vids": [
+	 {
+					"user": "TomasChikito",
+					"link": "",
+					"percent": 100,
+					"hz": "n/a"
+				},
+			],
+			"name": "Nine circles",
+                       "author": "Rasvaninx",
+                       "more": "Zobros",
+                       "id": 4284013,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 72,
+                       "verificationVid": "",
+                       "key": 0
+		},
+		/*=================================================================================*/
+			{
+			"vids": [
+	 {
+					"user": "TomasChikito",
+					"link": "",
+					"percent": 100,
+					"hz": "n/a"
+				},
+					 {
+					"user": "Seiryuu",
+					"link": "",
+					"percent": 100,
+					"hz": "n/a"
+				},
+			],
+			"name": "Jawbreaker",
+                       "author": "Rasvaninx",
+                       "more": "ZenthicAlpha",
+                       "id": 6939821,
+                       "pass": "ni idea :v usen el copy bypass",
+                       "percentToQualify": 65,
+                       "verificationVid": "",
+                       "key": 0
+		},
+	],
+	"version": [
+		1,
+		0,
+		0
+	]
+};
+
+const list = d.list;const version = d.version;
