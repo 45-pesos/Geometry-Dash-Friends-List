c5eb17bc095fb2773af170e386747ed3e78c862d
45-pesos <finosenoresycaquita666@gmail.com>
2026-09-06 21:24:01 -0600

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -124,6 +124,9 @@ export default {
                         Si haces un nivel y lo verificas, puede entrar a la lista siempre y cuando tenga la dificultad sufuciente para entrar y que tenga decoración (no necesariamente rateado, pero que se vea bien, aplica igual a otros niveles sin rate)
                     </p>
                     <p>
+                        La dificultad mínima para añadir a la lista es medium demon
+                    </p>
+                    <p>
                        Nota: el verificador de cada nivel será simplemente el primero de nosotros que se lo pase y lo mande al formulario
                     </p>
                 </div>
